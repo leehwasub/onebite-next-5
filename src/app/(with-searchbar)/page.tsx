6ed3527cf5a7d71ae0b84b5cee45b4c6d20bbf9c
hwasub <hwasub1115@naver.com>
2025-09-17 18:17:12 +0900

    6.3) 컴포넌트 스트리밍 적용하기

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -2,8 +2,10 @@ import BookItem from "@/components/book-item";
 import style from "./page.module.css";
 import books from "@/mock/books.json";
 import { BookData } from "@/types";
+import delay from "@/util/delay";
+import { Suspense } from "react";
 
-export const dynamic = 'auto';
+export const dynamic = 'force-dynamic';
 //특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
 // 1. auto : 기본값, 아무것도 강제하지 않음. 페이지의 유형을 자동으로 설정
 // 2. force-dynamic : 페이지의 유형을 Dynamic 페이지로 설정
@@ -11,6 +13,7 @@ export const dynamic = 'auto';
 // 4. error : 페이지의 유형을 Static 페이지로 설정 (설정하면 안되는 이유 -> 빌드 오류)
 
 async function AllBooks() {
+  await delay(1500);
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`, {cache: "force-cache"});
   if (!response.ok) {
     return <div>Error: {response.statusText}</div>;
@@ -22,6 +25,7 @@ async function AllBooks() {
 }
 
 async function RecoBooks() {
+  await delay(3000);
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`, 
     {next : {revalidate: 3}}
   );
@@ -39,11 +43,15 @@ export default async function Home() {
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <RecoBooks />
+        <Suspense fallback={<div>Loading...</div>}>
+          <RecoBooks />
+        </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <AllBooks />
+        <Suspense fallback={<div>Loading...</div>}>
+          <AllBooks />
+        </Suspense>
       </section>
     </div>
   );

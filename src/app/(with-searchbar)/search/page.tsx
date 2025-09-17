231aca1620f79a1b9bbb413183ca5834bd0c3d7f
hwasub <hwasub1115@naver.com>
2025-09-17 18:01:37 +0900

    6.2) 페이지 스트리밍 적용하기

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,6 +1,7 @@
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
+import delay from "@/util/delay";
 
 export default async function Page({
   searchParams,
@@ -8,6 +9,7 @@ export default async function Page({
   searchParams: Promise<{ q?: string }>;
 }) {
   const { q } = await searchParams;
+  await delay(1500);
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`, 
     {cache: "force-cache"});
   if (!response.ok) {

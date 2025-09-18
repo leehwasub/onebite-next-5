5e677057d474faa432ea50a249b90e82ff41a3c8
hwasub <hwasub1115@naver.com>
2025-09-18 21:48:01 +0900

    7.6) 클라이언트 컴포넌트에서의 서버액션

--- src/components/review-editor.tsx
+++ src/components/review-editor.tsx
@@ -1,16 +1,33 @@
+"use client";
+
 import style from "./review-editor.module.css";
 import { createReviewAction } from "@/actions/create-review-action";
+import { useActionState, useEffect } from "react";
 
 export default function ReviewEditor({bookId} : {bookId: string})
 {
+  const [state, formAction, isPending] = useActionState(
+    createReviewAction, 
+    null
+  );
+
+  useEffect(()=>{
+    if (state && state.status)
+    {
+      alert(state.error);
+    }
+  }, [state]);
+
   return (
     <section>
-      <form className={style.form_container} action={createReviewAction}>
-        <input name="bookId" value={bookId} hidden readOnly />
-        <textarea name="content" placeholder="리뷰 내용" required/>
+      <form className={style.form_container} action={formAction}>
+        <input disabled={isPending} name="bookId" value={bookId} hidden readOnly />
+        <textarea disabled={isPending} name="content" placeholder="리뷰 내용" required/>
         <div className={style.submit_container}>
           <input name="author" placeholder="작성자" required/>
-          <button type="submit">작성하기</button>  
+          <button disabled={isPending} type="submit">
+            {isPending ? "..." : "작성하기"}
+          </button>
         </div>
       </form>
     </section>

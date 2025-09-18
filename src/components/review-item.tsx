26c5e0b145f82563b2c73c8ba37e59f4f1be688a
hwasub <hwasub1115@naver.com>
2025-09-18 22:21:27 +0900

    7.7) 리뷰 삭제 기능 구현하기

--- src/components/review-item.tsx
+++ src/components/review-item.tsx
@@ -1,5 +1,6 @@
 import { ReviewData } from "@/types";
 import style from "./review-item.module.css";
+import ReviewItemDeleteButton from "./review-item-delete-button";
 
 export default function ReviewItem({id, content, author, createdAt, bookId} : ReviewData)
 {
@@ -9,7 +10,9 @@ export default function ReviewItem({id, content, author, createdAt, bookId} : Re
       <div className={style.content}>{content}</div>
       <div className={style.bottom_container}>
         <div className={style.date}>{new Date(createdAt).toLocaleString()}</div>
-        <div className={style.delete_btn}>삭제하기</div>
+        <div className={style.delete_btn}>
+          <ReviewItemDeleteButton reviewId={id} bookId={bookId} />
+        </div>
       </div>
     </div>
   );
